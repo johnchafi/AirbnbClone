cd9db0ca9ca6293074e91496a359aa233a075bb2
Jean de Dieu Uwimana <johnchafi@gmail.com>
2020-12-21 21:32:41 -0500

    Airbnb clone

---
+++ build/precache-manifest.a10945a0885660a033c618b797b4d400.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3039391bc82787a2fa1197e332564941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7325b01c46a1bfa87c",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "fec032ad1c17b183855e",
+    "url": "/static/css/main.8799c989.chunk.css"
+  },
+  {
+    "revision": "4b7325b01c46a1bfa87c",
+    "url": "/static/js/2.d774a216.chunk.js"
+  },
+  {
+    "revision": "dd35b97584ae2f76fec81c21e8577640",
+    "url": "/static/js/2.d774a216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec032ad1c17b183855e",
+    "url": "/static/js/main.58ef9685.chunk.js"
+  },
+  {
+    "revision": "c4ee4885d0aecf3d4e07",
+    "url": "/static/js/runtime-main.771cad96.js"
+  },
+  {
+    "revision": "ce613f068983006940b7b72b3098e886",
+    "url": "/static/media/banner4.ce613f06.jpg"
+  },
+  {
+    "revision": "e8d558e190eb18b94a6e0d4c59fcbb97",
+    "url": "/static/media/banner5.e8d558e1.jpg"
+  },
+  {
+    "revision": "ce613f068983006940b7b72b3098e886",
+    "url": "/static/media/banner6.ce613f06.jpg"
+  },
+  {
+    "revision": "bfe638cbd972fda9c188c52273aff907",
+    "url": "/static/media/banner7.bfe638cb.jpg"
+  },
+  {
+    "revision": "82aa3a37633e69be1137ba239da4462f",
+    "url": "/static/media/banner8.82aa3a37.jpg"
+  },
+  {
+    "revision": "76ead81e0a998842da8a24072af859ee",
+    "url": "/static/media/banner9.76ead81e.jpg"
+  }
+]);
